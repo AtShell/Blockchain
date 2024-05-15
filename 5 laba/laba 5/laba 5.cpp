7e214a375ff307f7cec4a343ba7472871c57b0fa
Вячеслав <76532024+AtShell@users.noreply.github.com>
2024-05-15 18:05:23 +0500

    Add files via upload

--- 5 laba/laba 5/laba 5.cpp
+++ 5 laba/laba 5/laba 5.cpp
@@ -1,77 +1,154 @@
-﻿#include <iostream>
-#include <vector>
-#include <mpi.h>
-#include <chrono>
-#include <random>
-double random_double(double min, double max) {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<double> res(min, max);
-    return res(gen);
+﻿#include <mpi.h>
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+const int N = 4800;
+
+double RandomDouble(double min, double max) {
+    return min + (max - min) * (rand() / static_cast<double>(RAND_MAX));
 }
-void random(std::vector<std::vector<double>>& A, std::vector<double>& b) {
-    int n = b.size();
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            A[i][j] = random_double(0, 10);
-        }
-        b[i] = random_double(0, 100);
-    }
-}
-void gauss(std::vector<std::vector<double>>& A, std::vector<double>& b) {
-    int n = b.size();
-    for (int i = 0; i < n; ++i) {
-        double pivot = A[i][i];
-        for (int j = i + 1; j < n; ++j) {
-            double ratio = A[j][i] / pivot;
-            for (int k = i; k < n; ++k) {
-                A[j][k] -= ratio * A[i][k];
-            }
-            b[j] -= ratio * b[i];
+
+
+double* FindY(double* A, double* X, int n) {
+    double* Y = (double*)malloc(n * sizeof(double));;
+    for (int i = 0; i < n; i++) {
+        Y[i] = 0;
+        for (int j = 0; j < n; j++) {
+            Y[i] += A[i * n + j] * X[j];
         }
     }
-    for (int i = n - 1; i >= 0; --i) {
-        double sum = 0.0;
-        for (int j = i + 1; j < n; ++j) {
-            sum += A[i][j] * b[j];
-        }
-        b[i] = (b[i] - sum) / A[i][i];
-    }
+    return Y;
 }
-int main(int argc, char** argv) {
-    setlocale(0, "ru");
+
+int main(int argc, char* argv[])
+{
+
+    double* X = new double[N];
+
+    int ProcAmout, rank;
+    MPI_Status status;
+    double beginTime, endTime;
     MPI_Init(&argc, &argv);
-    int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    int n;
-    int print;
-    std::cout << "Введи размер: ";
-    std::cin >> n;
-    std::cout << "Выводить решение? (1-да 0- нет)";
-    std::cin >> print;
-    std::vector<std::vector<double>> A(n, std::vector<double>(n));
-    std::vector<double> b(n);
-    if (rank == 0) {
-        random(A, b);
+    MPI_Comm_size(MPI_COMM_WORLD, &ProcAmout);
+
+    int blockSize = N / ProcAmout;
+
+    double* matrixA = NULL;
+    double* rightHandB = NULL;
+    double* X2 = NULL;
+    if (rank == 0) { 
+        matrixA = (double*)malloc(N * N * sizeof(double*));
+        for (int i = 0; i < N * N; i++) {
+            matrixA[i] = RandomDouble(-100, 100);
+        }
+        X2 = (double*)malloc(N * sizeof(double));
+        for (int i = 0; i < N; i++) {  
+            X2[i] = RandomDouble(-100, 100);
+        }
+        rightHandB = (double*)malloc(N * sizeof(double));
+        rightHandB = FindY(matrixA, X2, N);
     }
-    MPI_Bcast(&b[0], n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    for (int i = 0; i < n; ++i) {
-        MPI_Bcast(&A[i][0], n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    }
-    auto start_time = std::chrono::high_resolution_clock::now();
-    gauss(A, b);
-    auto end_time = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end_time - start_time;
+    double* subA = (double*)malloc(blockSize * N * sizeof(double*));
+    double* subB = (double*)malloc(blockSize * sizeof(double));
+
+    MPI_Scatter(matrixA, blockSize * N, MPI_DOUBLE, subA, blockSize * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Scatter(rightHandB, blockSize, MPI_DOUBLE, subB, blockSize, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+
+    double* row = (double*)malloc(N * sizeof(double));
+    double b;
+    int column;
+
     if (rank == 0) {
-        if (print == 1) {
-            std::cout << "Решение:\n";
-            for (size_t i = 0; i < b.size(); ++i) {
-                std::cout << "x[" << i << "] = " << b[i] << std::endl;
+        beginTime = MPI_Wtime();
+        for (int k = 0; k < blockSize; k++)
+        {
+            column = rank * blockSize + k;
+            for (int i = column + 1; i < N; i++) {
+                subA[k * N + i] = subA[k * N + i] / subA[k * N + column];
+            }
+            subB[k] /= subA[k * N + column];
+            subA[k * N + column] = 1;
+            b = subB[k];
+            memcpy(row, &subA[k * N], sizeof(double) * N);
+            MPI_Bcast(row, N, MPI_DOUBLE, rank, MPI_COMM_WORLD);
+            MPI_Bcast(&b, 1, MPI_DOUBLE, rank, MPI_COMM_WORLD);
+            for (int j = k + 1; j < blockSize; j++) {
+                for (int i = column + 1; i < N; i++) {
+                    subA[j * N + i] = subA[j * N + i] - subA[j * N + k] * row[i];
+
+                }
+                subB[j] -= subA[j * N + k] * b;
+                subA[j * N + column] = 0;
+
+            }
+
+        }
+
+    }
+    else { 
+        for (int k = 0; k < rank * blockSize; k++) {
+            MPI_Bcast(row, N, MPI_DOUBLE, k / blockSize, MPI_COMM_WORLD);
+            MPI_Bcast(&b, 1, MPI_DOUBLE, k / blockSize, MPI_COMM_WORLD);
+            for (int j = 0; j < blockSize; j++) {
+                for (int i = k + 1; i < N; i++) {
+                    subA[j * N + i] = subA[j * N + i] - subA[j * N + k] * row[i];
+                }
+                subB[j] -= subA[j * N + k] * b;
+                subA[j * N + k] = 0;
             }
         }
-        std::cout << "Затраченное время: " << elapsed_seconds.count() << " секунд" << std::endl;
+
+        for (int k = 0; k < blockSize; k++)
+        {
+            column = rank * blockSize + k;
+            for (int i = column + 1; i < N; i++) {
+                subA[k * N + i] = subA[k * N + i] / subA[k * N + column];
+            }
+            subB[k] /= subA[k * N + column];
+            subA[k * N + column] = 1;
+            b = subB[k];
+            memcpy(row, &subA[k * N], sizeof(double) * N);
+            MPI_Bcast(row, N, MPI_DOUBLE, rank, MPI_COMM_WORLD);
+            MPI_Bcast(&b, 1, MPI_DOUBLE, rank, MPI_COMM_WORLD);
+            for (int j = k + 1; j < blockSize; j++) {
+                for (int i = column + 1; i < N; i++) {
+                    subA[j * N + i] = subA[j * N + i] - subA[j * N + column] * row[i];
+
+                }
+                subB[j] -= subA[j * N + column] * b;
+                subA[j * N + column] = 0;
+            }
+
+        }
+
     }
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Gather(subA, blockSize * N, MPI_DOUBLE, matrixA, blockSize * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Gather(subB, blockSize, MPI_DOUBLE, rightHandB, blockSize, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    if (rank == 0) {
+        for (int k = N - 1; k >= 0; k--) {
+            X[k] = rightHandB[k];
+            for (int i = N - 1; i > k; i--) {
+                X[k] = X[k] - matrixA[k * N + i] * X[i];
+            }
+            X[k] = X[k] / matrixA[k * N + k];
+        }
+        endTime = MPI_Wtime();
+        std::cout << "Time = " << endTime - beginTime << std::endl;
+    }
+
     MPI_Finalize();
-    return 0;
+    if (rank == 0) {
+        int c = 0;
+        for (int i = 0; i < N; i++) {
+            if (std::abs(X[i] - X2[i]) > 0.00001) {
+                c++;
+            }
+        }
+        std::cout << "Number of non - matching X = " << c << std::endl;
+    }
+
 }
